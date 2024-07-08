3dcd7f00442e297353f1337e6ccec7e51304b6b3
Rytis-create <rytis.programer@gmail.com>
2024-07-08 15:30:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,26 @@ document.addEventListener('DOMContentLoaded', function() {
         currentColorIndex = (currentColorIndex + 1) % backgroundColors.length;
     }
 
-    setInterval(changeBackgroundColor, 3000); // Keisti foną kas 3 sekundes
+    setInterval(changeBackgroundColor, 3000);
+    document.addEventListener('DOMContentLoaded', function() {
+    const params = new URLSearchParams(window.location.search);
+    const lang = params.get('lang');
+
+    if (lang === 'en') {
+        translateToEnglish();
+    } else {
+        // Pagal nutylėjimą LT
+        translateToLithuanian();
+    }
+});
+
+function translateToEnglish() {
+    document.querySelectorAll('[data-lt]').forEach(function(el) {
+        el.textContent = el.getAttribute('data-en');
+    });
+}
+
+function translateToLithuanian() {
+    document.querySelectorAll('[data-en]').forEach(function(el) {
+        el.textContent = el.getAttribute('data-lt');// Keisti foną kas 3 sekundes
 });
