c091cbced1a72a001babe703c19df46fd1429fb7
Rytis-create <rytis.programer@gmail.com>
2024-07-08 15:35:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,14 +21,13 @@ document.addEventListener('DOMContentLoaded', function() {
         translateToLithuanian();
     }
 });
+function setLanguage(lang) {
+    const elements = document.querySelectorAll('[data-lt]');
 
-function translateToEnglish() {
-    document.querySelectorAll('[data-lt]').forEach(function(el) {
-        el.textContent = el.getAttribute('data-en');
+    elements.forEach(el => {
+        if (lang === 'lt') {
+            el.textContent = el.getAttribute('data-lt');
+        } else if (lang === 'en') {
+            el.textContent = el.getAttribute('data-en');
+        }
     });
-}
-
-function translateToLithuanian() {
-    document.querySelectorAll('[data-en]').forEach(function(el) {
-        el.textContent = el.getAttribute('data-lt');// Keisti foną kas 3 sekundes
-});
